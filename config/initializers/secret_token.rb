2309b4e2d0671b96483b89b488e7d520cfd0ddc3
unknown <vsingh@.corporate.thoughtworks.com>
2011-11-20 17:00:27 +0530

    Basic rails+refinrery application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruralindia::Application.config.secret_token = '98b915f681299d3c4534ee7d85b0becb60c303cc0b07d8f314a518395f4278b9ef3a80b715e0c509aa50d4c54cb4a021149c597143c14954b0fad9a2f58fb557'
