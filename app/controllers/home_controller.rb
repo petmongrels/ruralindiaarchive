741fbc80631160cd77d5ac21f153934fad47b3e4
unknown <vsingh@.corporate.thoughtworks.com>
2011-12-06 15:34:38 +0530

    Added custom settings for number of stories on home page and each story preview size.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ require "refinerycms-pages"
 
 class HomeController < ApplicationController
   def index
-    @recently_created_pages = Page.limit(10).order("created_at DESC")
+    @recently_created_pages = Page.limit(RefinerySetting.get(:number_of_stories_on_home_page)).order("created_at DESC")
   end
 end
---
+++ db/seeds/custom_refinery_settings.rb
@@ -0,0 +1,4 @@
+require "refinerycms-settings"
+
+RefinerySetting.set(:home_page_story_preview_size, 1000)
+RefinerySetting.set(:number_of_stories_on_home_page, 7)
